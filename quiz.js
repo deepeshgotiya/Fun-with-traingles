99e4624cee009503909cfb136c258329a2b65168
deepeshgotiya <deepeshgotiya79@gmail.com>
2021-11-21 10:07:37 +0530

    done with the problem two

---
+++ quiz.js
@@ -0,0 +1,23 @@
+const submitBtn = document.querySelector("#submit-answer")
+const scoreBox = document.querySelector("#outputDiv")
+const formDataValues = document.querySelector(".quiz-form")
+
+const rightAnswers = ["Midsegment","Equilateral"];
+
+function calcultateScore(){
+    let index = 0;
+    let score = 0;
+    const formResults = new FormData(formDataValues);
+    for(let value of formResults.values()){
+        if(value===rightAnswers[index]){
+            score= score+1;
+        }
+        index=index+1
+    }
+    scoreBox.innerHTML = "Your Score is "+score
+}
+
+
+
+
+submitBtn.addEventListener("click", calcultateScore)
