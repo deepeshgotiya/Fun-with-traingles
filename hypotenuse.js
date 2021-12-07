d9cbf1f586d08216efca551fa132a3d5f5a24ab1
deepeshgotiya <deepeshgotiya79@gmail.com>
2021-12-07 16:36:58 +0530

    fixed-negative-values

--- hypotenuse.js
+++ hypotenuse.js
@@ -3,13 +3,21 @@ const sideInput = document.querySelectorAll(".side-value");
 const output = document.querySelector("#H-output");
 
 function calculateSumOfSides(a,b){
-    const sumOfsquare = a*a + b*b ;
-    return sumOfsquare;
+    if(a<0 || b<0){
+        output.innerText = "Enter a valid input"
+    }else{
+        const sumOfsquare = a*a + b*b ;
+        return sumOfsquare;
+    }
 }
 
 function calculateHypotanuse (){
-    const sumOfsquare = calculateSumOfSides(Number(sideInput[0].value),Number(sideInput[1].value));
-    const lenghtOfHypotenuse = Math.sqrt(sumOfsquare);
-    output.innerText = "The sum of hypotenuse is " + lenghtOfHypotenuse
+    // if(Number(sideInput[0].value)> 0 ||  Number(sideInput[1].value)>0){
+        const sumOfsquare = calculateSumOfSides(Number(sideInput[0].value),Number(sideInput[1].value));
+        const lenghtOfHypotenuse = Math.sqrt(sumOfsquare);
+        output.innerText = "The sum of hypotenuse is " + lenghtOfHypotenuse
+    // }else{
+    //     output.innerText = "Enter a valid input"
+    // }
 }
 resultBtn.addEventListener("click", calculateHypotanuse)
